5d2d9ff3879d6964f01d20fd227ce1aadacdcba8
Fuge <zsolt.fugedi@barre.hu>
2020-12-23 20:12:04 +0100

    scripting visual studio code-al, kamera mozgatás

---
+++ Assets/misc/Hello.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log( "Hello Fuge at Start()!" );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards( transform.position,new Vector3( 0,0,0 ),Time.deltaTime );
+    }
+}
