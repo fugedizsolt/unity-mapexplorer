4ef4d59cb629a70f05b91dbf2440ce803875fe1c
Fuge <zsolt.fugedi@barre.hu>
2020-12-25 18:50:40 +0100

    posInfo kiírás a képernyőn

--- Assets/misc/MyCamera.cs
+++ Assets/misc/MyCamera.cs
@@ -17,11 +17,13 @@ public class MyCamera : MonoBehaviour
 	private readonly float	MOUSE_SCALE		= 60.0f/20.0f;	// 100 pixelenkent 60 fok/sec
 	private readonly float	VELOCITY_DEGREE_MULT	= (float)(1.0f/180.0f*Mathf.PI);	// fok/sec, radianban 2PI/60 /sec
 
-    private Vector3	location;
+	private bool bUseMouseInput = false;
+
+	private Vector3	location;
 	private Quaternion	quat = new Quaternion();
 	private Quaternion	tmpquat = new Quaternion();
 
-    private Vector3	mouseCenter;
+	private Vector3	mouseCenter;
 
 	private float		velocityYaw = 0.0f;		// fok/sec
 	private float		velocityPitch = 0.0f;	// fok/sec
@@ -35,85 +37,109 @@ public class MyCamera : MonoBehaviour
 	private Vector3	vecMoveRight = new Vector3( 0,0,0 );
 	private Vector3	vecMoveUp = new Vector3( 0,0,0 );
 
-    private ManagerGyorsulasSebesseg	gyorsulasForward;
-    private ManagerGyorsulasSebesseg	gyorsulasRight = null;
-    private ManagerGyorsulasSebesseg	gyorsulasUp = null;
-    private ManagerGyorsulasSebesseg	gyorsulasRotate = null;
+	private ManagerGyorsulasSebesseg	gyorsulasForward;
+	private ManagerGyorsulasSebesseg	gyorsulasRight = null;
+	private ManagerGyorsulasSebesseg	gyorsulasUp = null;
+	private ManagerGyorsulasSebesseg	gyorsulasRotate = null;
 
-    private ACC_DIR_TYPE prevDir = ACC_DIR_TYPE.FORWARD;
-    private int counter = 0;
+	private ACC_DIR_TYPE prevDir = ACC_DIR_TYPE.FORWARD;
+	private int counter = 0;
 
+	private UnityEngine.UI.Text objPosInfo;
 
-    void Start()
-    {
-        Debug.Log( "Hello Fuge at Start()!" );
-        //location = transform.position;
+	void Start()
+	{
+		Debug.Log( "Hello Fuge at Start()!" );
+		//location = transform.position;
 
-        //vecMoveForward = transform.forward;
+		//vecMoveForward = transform.forward;
 		//gyorsulasForward = new ManagerGyorsulasSebesseg( true );
-        gyorsulasForward = ScriptableObject.CreateInstance<ManagerGyorsulasSebesseg>();
+		gyorsulasForward = ScriptableObject.CreateInstance<ManagerGyorsulasSebesseg>();
 		gyorsulasRight = ScriptableObject.CreateInstance<ManagerGyorsulasSebesseg>();
 		gyorsulasUp = ScriptableObject.CreateInstance<ManagerGyorsulasSebesseg>();
 		gyorsulasRotate = ScriptableObject.CreateInstance<ManagerGyorsulasSebesseg>();
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        counter++;
-        if ( counter<10 )
-            Debug.Log( "Time.deltaTime:" + Time.deltaTime + " realtimeSinceStartup:" + Time.realtimeSinceStartup );
+	// Update is called once per frame
+	void Update()
+	{
+		if ( objPosInfo==null )
+			objPosInfo = GameObject.FindGameObjectWithTag( "posInfo" ).GetComponent<UnityEngine.UI.Text>();
+
+		objPosInfo.text = string.Format( 
+			"counter:{0}\n" + 
+			"position:{1,0:F2},{2,0:F2},{3,0:F2}\n" +
+			"forward:{4,0:F2}\n" +
+			"right:{5,0:F2}\n" +
+			"up:{6,0:F2}\n",
+			counter,
+			transform.position.x,
+			transform.position.y,
+			transform.position.z,
+			0,0,0 );
+
+		counter++;
+		if ( counter<10 )
+			Debug.Log( "Time.deltaTime:" + Time.deltaTime + " realtimeSinceStartup:" + Time.realtimeSinceStartup );
+
+		if ( counter==1 )
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Debug.Log( "Locked Input.mousePosition.x=" + Input.mousePosition.x + " y=" + Input.mousePosition.y );
+		}
+		//if ( counter%60==0 ) Debug.Log( "Input.mousePosition.x=" + Input.mousePosition.x + " y=" + Input.mousePosition.y );
 
 		float deltaTime = Time.deltaTime;
-        //float velocityForward = gyorsulasForward.updateVelocity( deltaTime );
-        //float velocityRight = gyorsulasRight.updateVelocity( deltaTime );
-        //float velocityUp = gyorsulasUp.updateVelocity( deltaTime );
-        //float velocityRoll = gyorsulasRotate.updateVelocity( deltaTime ) * SCALE_ROTATE;
+		//float velocityForward = gyorsulasForward.updateVelocity( deltaTime );
+		//float velocityRight = gyorsulasRight.updateVelocity( deltaTime );
+		//float velocityUp = gyorsulasUp.updateVelocity( deltaTime );
+		//float velocityRoll = gyorsulasRotate.updateVelocity( deltaTime ) * SCALE_ROTATE;
 
-        if ( counter==10 ) //Input.GetKeyDown(KeyCode.A) )
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Debug.Log( "Locked Input.mousePosition.x=" + Input.mousePosition.x + " y=" + Input.mousePosition.y );
-        }
+		if ( Input.GetKeyDown( KeyCode.S )==true )
+		{
+			bUseMouseInput = !bUseMouseInput;
+			if ( Cursor.lockState==CursorLockMode.Locked )
+			{
+				Cursor.lockState = CursorLockMode.None;
+				mouseCenter = Input.mousePosition;
+				Debug.Log( "None Input.mousePosition.x=" + Input.mousePosition.x + " y=" + Input.mousePosition.y );
+			}
+		}
+		else
+		{
+			if ( bUseMouseInput==true )
+			{
+				setYawPitchFromMousePos();
 
-        if ( counter==20 ) //Input.GetKeyDown(KeyCode.A) )
-        {
-            Cursor.lockState = CursorLockMode.None;
-            mouseCenter = Input.mousePosition;
-            Debug.Log( "None Input.mousePosition.x=" + Input.mousePosition.x + " y=" + Input.mousePosition.y );
-        }
+				//velocityRoll = 100f;
+				//velocityYaw = 20f;
 
-        if ( counter%60==0 ) Debug.Log( "Input.mousePosition.x=" + Input.mousePosition.x + " y=" + Input.mousePosition.y );
+				float segedFloat = VELOCITY_DEGREE_MULT * deltaTime;
+				tmpquat = Quaternion.Euler( 
+					velocityPitch*segedFloat,
+					velocityYaw*segedFloat,
+					velocityRoll*segedFloat );
 
-        if ( counter>100 ) //Input.GetKeyDown(KeyCode.A) )
-            setYawPitchFromMousePos();
+				transform.rotation *= tmpquat;
 
-		//velocityRoll = 100f;
-        //velocityYaw = 20f;
+				//Matrix4x4 m4x4rot = Matrix4x4.Rotate( transform.rotation );
+				//Matrix4x4 m4x4rottmp = m4x4rot * Matrix4x4.Rotate( tmpquat );
+				//transform.rotation = m4x4rottmp.rotation;
 
-		float segedFloat = VELOCITY_DEGREE_MULT * deltaTime;
-		tmpquat = Quaternion.Euler( 
-			velocityPitch*segedFloat,
-			velocityYaw*segedFloat,
-			velocityRoll*segedFloat );
+				Vector3 vecForward = transform.rotation * Vector3.forward;
+				//Matrix4x4.Rotate( quat );
 
-        transform.rotation *= tmpquat;
+				transform.position += vecForward * (deltaTime/10);
+			}
+		}
 
-        //Matrix4x4 m4x4rot = Matrix4x4.Rotate( transform.rotation );
-        //Matrix4x4 m4x4rottmp = m4x4rot * Matrix4x4.Rotate( tmpquat );
-		//transform.rotation = m4x4rottmp.rotation;
-
-        Vector3 vecForward = transform.rotation * Vector3.forward;
-        //Matrix4x4.Rotate( quat );
-
-        transform.position += vecForward * (deltaTime/10);
-    }
+	}
 
 	public void setYawPitchFromMousePos()
 	{
 		float   dax,day;
 
-        Vector3 mouseDiff = Input.mousePosition -  mouseCenter;
+		Vector3 mouseDiff = Input.mousePosition -  mouseCenter;
 		float mx = mouseDiff.x;
 		float my = mouseDiff.y;
 
