7cba9e510c58e7426b97930dec321df8a9039116
Fuge <zsolt.fugedi@barre.hu>
2020-12-27 15:52:00 +0100

    elképesztően szép programot készítettem, működik, gyönyörű!

--- Assets/misc/MesgGen1.cs
+++ Assets/misc/MesgGen1.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class MesgGen1 : MonoBehaviour
 {
     private Mesh mesh;
-	private readonly int gridSize = 1024;
+	private readonly int gridSize = 200;
 
 	// Start is called before the first frame update
 	void Start()
@@ -15,36 +15,82 @@ public class MesgGen1 : MonoBehaviour
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = this.mesh;
 
-        CreateFixShape();
-        //CreateSeattleMap();
+        //CreateFixShape();
+        CreateSeattleMap();
     }
 
 	private void CreateSeattleMap()
     {
+		string pathToMap = "map1024x1024";
+		TextAsset assetMap = Resources.Load<TextAsset>( pathToMap );
+		string pathToCol = "col1024x1024";
+		TextAsset assetCol = Resources.Load<TextAsset>( pathToCol );
+		byte[] arrayDataMap = assetMap.bytes;
+		byte[] arrayDataCol = assetCol.bytes;
 
+        var texture = new Texture2D( gridSize+1,gridSize+1,TextureFormat.ARGB32,false );
+        Vector3[] vertices = new Vector3[(gridSize + 1)*(gridSize + 1)];
+        for (int z = 0; z <= gridSize; z++)
+        {
+            for (int x = 0; x <= gridSize; x++)
+            {
+                byte data = arrayDataMap[z*1024+x];
+                float valh = (float)data/255f*30f;   // max 3 magas lesz
+                int indexCol = z*1024*3+3*x;
+                if ( indexCol+2<arrayDataCol.Length )
+                {
+                    byte colr = arrayDataCol[indexCol];
+                    byte colg = arrayDataCol[indexCol+1];
+                    byte colb = arrayDataCol[indexCol+2];
+                    texture.SetPixel( x,z,new Color( (float)colr/256f,(float)colg/256f,(float)colb/256f ) );
+                }
+                else
+                {
+                    texture.SetPixel( x,z,Color.black );
+                }
+
+                vertices[x+z*(gridSize+1)] = new Vector3( x,valh,z );
+            }
+        }
+        texture.Apply();
+        // connect texture to material of GameObject this script is attached to
+        GetComponent<Renderer>().material.mainTexture = texture;
+
+		int[] triangles = generateIndexBuffer( gridSize+1 );
+
+        Vector2[] uvs = new Vector2[vertices.Length];
+        for (int i = 0; i < uvs.Length; i++)
+        {
+            uvs[i] = new Vector2(vertices[i].x/gridSize, vertices[i].z/gridSize);
+        }
+
+        this.mesh.Clear();
+        //this.mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        this.mesh.vertices = vertices;
+        this.mesh.triangles = triangles;
+        this.mesh.uv = uvs;
+        this.mesh.RecalculateNormals();
     }
 
 	private void CreateFixShape()
 	{
         var texture = new Texture2D( gridSize+1,gridSize+1,TextureFormat.ARGB32,false );
- 
-        // connect texture to material of GameObject this script is attached to
-        GetComponent<Renderer>().material.mainTexture = texture;
-
-		Vector3[] vertices = new Vector3[(gridSize + 1)*(gridSize + 1)];
-		for (int z = 0; z <= gridSize; z++)
+        Vector3[] vertices = new Vector3[(gridSize + 1)*(gridSize + 1)];
+        for (int z = 0; z <= gridSize; z++)
         {
-			for (int x = 0; x <= gridSize; x++)
+            for (int x = 0; x <= gridSize; x++)
             {
-				float valh = getHeight(x, z);
+                float valh = getHeight(x, z);
                 if ( valh<=0.1 ) texture.SetPixel( x, z, Color.blue );
                 else if ( valh>1.9 ) texture.SetPixel( x, z, Color.red );
                 else texture.SetPixel( x, z, Color.green );
 
                 vertices[x+z*(gridSize+1)] = new Vector3( x,valh,z );
-			}
-		}
+            }
+        }
         texture.Apply();
+        // connect texture to material of GameObject this script is attached to
+        GetComponent<Renderer>().material.mainTexture = texture;
 
 		int[] triangles = generateIndexBuffer( gridSize+1 );
 
